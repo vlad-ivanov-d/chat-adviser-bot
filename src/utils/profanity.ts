72b310e189ffb0ccc8bda2f052d69494f2d7fb91
Vlad Ivanov <vlad.ivanov.d@gmail.com>
2023-10-03 00:37:25 +0300

    imo: profanity filter

--- src/utils/profanity.ts
+++ src/utils/profanity.ts
@@ -22,6 +22,46 @@ export interface ProfaneWord {
 
 export class Profanity {
   private readonly profaneWords: ProfaneWord[];
+  /**
+   * English similar chars which can be used to workaround filter. Chars must be defined in lower case.
+   */
+  private readonly similarCharsEn: Record<string, string[]> = {};
+  /**
+   * Russian similar chars which can be used to workaround filter. Chars must be defined in lower case.
+   */
+  private readonly similarCharsRu: Record<string, string[]> = {
+    а: ["a", "@"],
+    б: ["b", "6"],
+    в: ["b", "v"],
+    г: ["g", "r"],
+    д: ["d", "g"],
+    е: ["e", "ё"],
+    ж: ["zh", "*"],
+    з: ["z", "3"],
+    и: ["i", "u"],
+    й: ["i", "u", "y", "и"],
+    к: ["k", "i{", "|{"],
+    л: ["l", "ji"],
+    м: ["m"],
+    н: ["h", "n"],
+    о: ["o", "0"],
+    п: ["п", "n", "p"],
+    р: ["p", "r"],
+    с: ["c", "s", "$"],
+    т: ["m", "t"],
+    у: ["u", "y"],
+    ф: ["f"],
+    х: ["h", "x", "}{"],
+    ц: ["c", "u,"],
+    ч: ["ch"],
+    ш: ["sh"],
+    щ: ["sch"],
+    ы: ["bi"],
+    ь: ["b"],
+    э: ["e", "е"],
+    ю: ["io"],
+    я: ["ya"],
+  };
 
   /**
    * Creates the class to work with profane words
@@ -29,8 +69,8 @@ export class Profanity {
    */
   public constructor(profaneWords: Array<ProfaneWord | string>) {
     this.profaneWords = profaneWords.map((w) => ({
-      isRoot: typeof w === "object" ? w.isRoot : undefined,
-      word: this.removeDuplicateChars(typeof w === "string" ? w.toLowerCase() : w.word.toLowerCase()),
+      isRoot: typeof w === "string" ? undefined : w.isRoot,
+      word: typeof w === "string" ? w : w.word,
     }));
   }
 
@@ -41,37 +81,74 @@ export class Profanity {
    */
   public filter(text: string): ProfanityResult {
     let hasProfanity = false;
-    const filteredText = text
-      .split(/\s+/)
+    const filteredText = this.splitText(text)
       .map((word) => {
-        const cleanedWord = this.cleanWord(word);
-        if (this.isProfane(cleanedWord)) {
+        let hasWord = false;
+        /**
+         * Replaces profanity with * chars and sets corresponding flags
+         * @returns Replaced string
+         */
+        const replacer = (): string => {
           hasProfanity = true;
-          return this.replaceProfanity(cleanedWord);
+          hasWord = true;
+          return "*".repeat(word.length);
+        };
+        const cleanWord = this.cleanWord(word);
+        for (const profaneWord of this.profaneWords) {
+          const cleanProfaneWord = this.cleanWord(profaneWord.word);
+          const regExpEn = this.getProfanityRegExp({ ...profaneWord, word: cleanProfaneWord }, this.similarCharsEn);
+          const regExpRu = this.getProfanityRegExp({ ...profaneWord, word: cleanProfaneWord }, this.similarCharsRu);
+          const filteredWord = cleanWord.replace(regExpEn, replacer).replace(regExpRu, replacer);
+          if (hasWord) {
+            return filteredWord;
+          }
         }
         return word;
       })
-      .join(" ");
+      .join("");
     return { filteredText, hasProfanity };
   }
 
   /**
-   * Removes punctuation characters and converting the word to lowercase
+   * Removes duplicated chars and converting the word to lowercase
    * @param word Word to clean
    * @returns Clean word
    */
   private cleanWord(word: string): string {
-    return word.toLowerCase().replace(/[^\wа-яА-ЯёЁ]/gi, "");
+    return this.removeDuplicateChars(word.toLowerCase());
   }
 
   /**
-   * Checks if the word is profane
-   * @param word Word to check
-   * @returns True if the word is profane
+   * Escapes char if it's necessary for correct regular expression
+   * @param char Char which should be escaped if necessary
+   * @returns Escaped char
    */
-  private isProfane(word: string): boolean {
-    const cleanWord = this.removeDuplicateChars(word);
-    return this.profaneWords.some((w) => (w.isRoot ? cleanWord.includes(w.word) : w.word === word));
+  private escapeCharForRegExp(char: string): string {
+    const escapeChars: string[] = ["*", "$"];
+    return char
+      .split("")
+      .map((c) => (escapeChars.includes(c) ? `\\${c}` : c))
+      .join("");
+  }
+
+  /**
+   * Gets profanity regular expression
+   * @param profaneWord Profane word object
+   * @param similarChars Similar chars which can be used to workaround regular expression
+   * @returns Profanity regular expression
+   */
+  private getProfanityRegExp(profaneWord: ProfaneWord, similarChars: Record<string, string[]>): RegExp {
+    const profanityRegexStr = profaneWord.word
+      .split("")
+      .map((char) => {
+        const chars = [this.escapeCharForRegExp(char)];
+        if (similarChars[char]) {
+          chars.push(...similarChars[char].map((c) => this.escapeCharForRegExp(c)));
+        }
+        return `(${chars.join("|")})`;
+      })
+      .join("");
+    return new RegExp(profaneWord.isRoot ? profanityRegexStr : `^${profanityRegexStr}$`);
   }
 
   /**
@@ -93,11 +170,14 @@ export class Profanity {
   }
 
   /**
-   * Replaces profane word with * characters
-   * @param word Word to replace
-   * @returns Word replaced with * characters
+   * Splits text into words and punctuation
+   * @param text Text to split
+   * @returns An array of separate words and puncruation
    */
-  private replaceProfanity(word: string): string {
-    return "*".repeat(word.length);
+  private splitText(text: string): string[] {
+    // Regular expression to separate text into words, punctuation and spaces.
+    const wordsAndPunctuation = text.split(/([\s,.;!?]+)/);
+    // Removing empty elements that could appear due to several separators in a row
+    return wordsAndPunctuation.filter((item) => item !== "");
   }
 }
