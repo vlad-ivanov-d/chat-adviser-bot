ac554258717ed3ea5bf2fa1d244045dc546de55a
Vlad Ivanov <vlad.ivanov.d@gmail.com>
2023-11-25 02:34:53 +0300

    oth: coverae threshold

--- jest.config.js
+++ jest.config.js
@@ -2,6 +2,14 @@ const { compilerOptions } = require("./tsconfig.json");
 
 /** @type {import('jest').Config} */
 const config = {
+  coverageThreshold: {
+    global: {
+      branches: 10,
+      functions: 10,
+      lines: 10,
+      statements: 10,
+    },
+  },
   modulePaths: [compilerOptions.baseUrl],
   preset: "ts-jest",
   setupFiles: ["<rootDir>/src/tests/setEnvVars.ts"],
