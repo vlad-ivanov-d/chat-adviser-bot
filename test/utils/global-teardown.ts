bd6a659d70d3f048905fe4735c573cec1c679fac
Vlad Ivanov <vlad.ivanov.d@gmail.com>
2024-02-09 00:31:47 +0300

    oth: update test config

--- test/utils/global-teardown.ts
+++ test/utils/global-teardown.ts
@@ -2,13 +2,23 @@ import { execSync } from "node:child_process";
 
 import type { Config } from "jest";
 
+let isEventAdded = false;
+
 /**
  * Allows the use of a custom global teardown module. The function will be triggered once after all test suites.
  * @param config Jest config
  */
 const globalTeardown = (config: Config): void => {
   const command = "docker compose -f compose.test.yml down";
-  config.watch === true || config.watchAll ? process.once("exit", () => execSync(command)) : execSync(command);
+  if (isEventAdded) {
+    return;
+  }
+  if (config.watch === true || config.watchAll) {
+    process.once("exit", () => execSync(command));
+    isEventAdded = true;
+    return;
+  }
+  execSync(command);
 };
 
 // Jest global teardown should have default export
