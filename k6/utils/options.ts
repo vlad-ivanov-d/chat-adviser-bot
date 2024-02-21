44688f7e4c71b02f6ca9e1c98fabec9740a87a8d
Vlad Ivanov <vlad.ivanov.d@gmail.com>
2024-02-21 03:10:48 +0200

    oth: update k6 thresholds

--- k6/utils/options.ts
+++ k6/utils/options.ts
@@ -5,7 +5,7 @@ export const options = {
   duration: "15s",
   thresholds: {
     checks: ["rate===1"], // 100% of successful checks
-    http_req_duration: ["p(100)<200"], // 100% of requests should be below 200ms
+    http_req_duration: ["p(100)<300"], // 100% of requests should be below 300ms
   },
   vus: 50,
 };
