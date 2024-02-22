d4e5c35e04b4a82992310f9094dd48cbeae68f37
Vlad Ivanov <vlad.ivanov.d@gmail.com>
2024-02-22 22:57:13 +0200

    oth: update tests

--- test/warnings.e2e-spec.ts
+++ test/warnings.e2e-spec.ts
@@ -170,6 +170,45 @@ describe("WarningsModule (e2e)", () => {
     expect(deleteMessagePayload).toEqual({ chat_id: supergroup.id, message_id: 3 });
   });
 
+  it("should not fail without ban and delete message permissions", async () => {
+    await createDbSupergroupChat({ hasWarnings: true });
+    await prisma.$transaction([
+      createDbUser(),
+      prisma.warning.createMany({
+        data: [
+          { authorId: adminUser.id, chatId: supergroup.id, editorId: adminUser.id, messageId: 1, userId: user.id },
+          { authorId: adminUser.id, chatId: supergroup.id, editorId: adminUser.id, messageId: 2, userId: user.id },
+        ],
+      }),
+    ]);
+    let banChatMemberPayload;
+    let deleteMessagePayload;
+    server.use(
+      http.post(`${TELEGRAM_API_BASE_URL}/banChatMember`, async (info) => {
+        banChatMemberPayload = await info.request.json();
+        return new HttpResponse(null, { status: 400 });
+      }),
+      http.post(`${TELEGRAM_API_BASE_URL}/deleteMessage`, async (info) => {
+        deleteMessagePayload = await info.request.json();
+        return new HttpResponse(null, { status: 400 });
+      }),
+      http.post(`${TELEGRAM_API_BASE_URL}/sendMessage`, () =>
+        HttpResponse.json({ ok: true, result: { message_id: 5 } }),
+      ),
+    );
+
+    const response = await request(TEST_WEBHOOK_BASE_URL).post(TEST_WEBHOOK_PATH).send(fixtures.warnWebhook);
+
+    expect(response.status).toBe(200);
+    expect(response.body).toEqual({ chat_id: supergroup.id, message_id: 4, method: "deleteMessage" });
+    await sleep(ASYNC_REQUEST_DELAY);
+    expect(banChatMemberPayload).toEqual({ chat_id: supergroup.id, user_id: user.id });
+
+    jest.runOnlyPendingTimers();
+    await sleep(ASYNC_REQUEST_DELAY);
+    expect(deleteMessagePayload).toEqual({ chat_id: supergroup.id, message_id: 3 });
+  });
+
   it("should render settings", async () => {
     await createDbSupergroupChat();
     let editMessageTextPayload;
