86311874bcc38b888fd3d53631b7e00d80c4d3ad
Vlad Ivanov <vlad.ivanov.d@gmail.com>
2024-03-06 21:25:45 +0200

    oth: update k6 tests

--- k6/channel-message-filter.spec.ts
+++ k6/channel-message-filter.spec.ts
@@ -3,14 +3,25 @@ import http from "k6/http";
 import * as fixtures from "test/fixtures/channel-message-filter";
 
 import { K6_WEBHOOK_URL } from "./utils/constants";
-import { options } from "./utils/options";
-
-export { options };
 
 /**
- * Virtual user code
+ * Test options
  */
-export default (): void => {
+export const options = {
+  discardResponseBodies: true,
+  scenarios: {
+    channel_message_filter: { duration: "10s", exec: "channelMessageFilter", executor: "constant-vus", vus: 50 },
+  },
+  thresholds: {
+    checks: ["rate===1"], // 100% of successful checks
+    http_req_duration: ["p(100)<300"], // 100% of requests should be below 300ms
+  },
+};
+
+/**
+ * It should filter channel messages in a new supergroup chat
+ */
+export const channelMessageFilter = (): void => {
   const res = http.post(K6_WEBHOOK_URL, JSON.stringify(fixtures.channelMessageWebhook));
   check(res, {
     /**
