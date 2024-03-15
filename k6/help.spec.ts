600ea3a41c2a521ecf7431ba9a8014198661aedb
Vlad Ivanov <vlad.ivanov.d@gmail.com>
2024-03-16 00:19:46 +0200

    oth: update load tests

--- k6/help.spec.ts
+++ k6/help.spec.ts
@@ -15,7 +15,7 @@ export const options = {
   },
   thresholds: {
     checks: ["rate===1"], // 100% of successful checks
-    http_req_duration: ["p(100)<300"], // 100% of requests should be below 300ms
+    http_req_duration: ["p(95)<300"], // 95% of requests should be below 300ms
   },
 };
 
